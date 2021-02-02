dd86ec9a4b1de36d7e85759778eeac6fcaa8811c
ELASRI MOHAMMED <1mohamedelasri@gmail.com>
2021-02-02 05:35:17 +0100

    [DONE] new APIs authentication

--- src/main/java/com/devel/stillcareBackend/repositories/ContactRepository.java
+++ src/main/java/com/devel/stillcareBackend/repositories/ContactRepository.java
@@ -1,9 +1,17 @@
 package com.devel.stillcareBackend.repositories;
 import com.devel.stillcareBackend.model.ContactEntity;
+import com.devel.stillcareBackend.model.PersonnelEntity;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
 
 
 @Repository
 public interface ContactRepository extends JpaRepository<ContactEntity,Long>{
+    @Query("select cnt from ContactEntity cnt where cnt.firebasetoken = :token ")
+    public Optional<ContactEntity> authenticateContact(@Param("token") String token);
+
 }
