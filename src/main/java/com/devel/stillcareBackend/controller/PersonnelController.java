9efc6d95a99940be4b1489d9b3cb1818a85be169
ELASRI MOHAMMED <1mohamedelasri@gmail.com>
2021-02-02 01:55:08 +0100

    [DONE] Personnel authentication

--- src/main/java/com/devel/stillcareBackend/controller/PersonnelController.java
+++ src/main/java/com/devel/stillcareBackend/controller/PersonnelController.java
@@ -1,11 +1,13 @@
 package com.devel.stillcareBackend.controller;
 
+import com.devel.stillcareBackend.exception.exceptionmodels.BadParametersException;
 import com.devel.stillcareBackend.exception.exceptionmodels.NotFoundException;
 import com.devel.stillcareBackend.model.PersonnelEntity;
 import com.devel.stillcareBackend.repositories.PersonnelRepository;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Map;
 
 @RestController
 public class PersonnelController {
@@ -24,13 +26,21 @@ public class PersonnelController {
         return repository.findAll();
     }
     // end::get-aggregate-root[]
-
     @PostMapping("/personnels")
     PersonnelEntity newPersonnel(@RequestBody PersonnelEntity newPersonnel) {
         return repository.save(newPersonnel);
     }
 
-    // Single item
+
+    @CrossOrigin(origins = "http://localhost:4200")
+    @PostMapping("/personnels/athentication")
+    PersonnelEntity one(@RequestBody Map<String,String> playload) {
+        if(playload == null) throw new BadParametersException("login : " + playload);
+        return repository.authenticatePersonnel(playload.get("mail"),playload.get("password"))
+                .orElseThrow(()-> new NotFoundException("User or password are incorrect"));
+    }
+
+
 
     @GetMapping("/personnels/{id}")
     PersonnelEntity one(@PathVariable Long id) {
--- src/main/java/com/devel/stillcareBackend/repositories/PersonnelRepository.java
+++ src/main/java/com/devel/stillcareBackend/repositories/PersonnelRepository.java
@@ -1,10 +1,16 @@
 package com.devel.stillcareBackend.repositories;
 import com.devel.stillcareBackend.model.PersonnelEntity;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
 
 
 @Repository
 public interface PersonnelRepository extends JpaRepository<PersonnelEntity,Long>{
+    @Query("select pers from PersonnelEntity pers where pers.login = :username and pers.password = :password ")
+    public Optional<PersonnelEntity> authenticatePersonnel(@Param("username") String username, @Param("password") String password);
 
 }
